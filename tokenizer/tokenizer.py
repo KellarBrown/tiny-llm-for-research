d5b2d7b1e07c78aff04b570bcaa9390d5a9b24b3
KellarBrown <kb82790@gmail.com>
2025-06-19 22:17:03 -0500

    build_vocab() complete

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -19,20 +19,20 @@ class Tokenizer:
         text = " ".join([self.int_to_str[i] for i in ids]) 
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
         return text
-    
-    def build_vocab(cls, text:str, min_freq: int = 1):
+    @classmethod
+    def build_vocab(cls,text:str, min_freq: int = 1):
         tokens = re.split(r'([,.:;?_!"()\']|--|\s)', text)
         tokens = [t.strip() for t in tokens if t.strip()]
         counter = Counter(tokens)
         filtered_tokens = [token for token, freq in counter.items() if freq >= min_freq]
-        filtered_tokens.append("<unk>")
-        vocab = {token: idx for idx, token in enumerate(filter)}
+        if "<unk>" not in filtered_tokens:
+            filtered_tokens.append("<unk>")
+        vocab = {token: idx for idx, token in enumerate(filtered_tokens)}
         return cls(vocab)
     
 if __name__ == "__main__":
-    vocab = {"Hello": 0, "world": 1, "<unk>": 2}
-    tokenizer = Tokenizer(vocab)
 
-    print("Encoded:", tokenizer.encode("Hello you beautiful world"))
-    print("Decoded:", tokenizer.decode([0, 2, 2, 1]))
-    
+    sample_text = "Friend, Romans, countrymen, lend me your ears. dlkfjdk"
+    tokenizer = Tokenizer.build_vocab(sample_text, min_freq=2)
+    print("Vocab:", tokenizer.str_to_int)
+    print("Encode:", tokenizer.encode("Friend, Romans, countrymen, lend me your ears. dlkfjdk"))
